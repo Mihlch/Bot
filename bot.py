ceb1b5ba69d01ad79a41fb312ea698ab15209945
Mihlch <miki171028@gmail.com>
2021-01-31 12:21:31 +0700

    n

--- bot.py
+++ bot.py
@@ -1,6 +1,20 @@
-In = open('input.txt', 'r')
-Out = open('output.txt', 'w')
-n = In.readline(1)
-print(n)
-In.close()
-Out.close()
+import sys
+
+if __name__ == '__main__':
+    In = open(sys.argv[1], 'r')
+    Out = open(sys.argv[2], 'w')
+    n1 = In.readline()
+    n2 = In.readline()
+    n3 = In.readline()
+    n4 = In.readline()
+    print(n3, n4, "gggggggggggg")
+    n5 = In.readline()
+    n6 = In.readline()
+    n7 = In.readline()
+    hod =(0, 0)
+    #if n3[0] == -1 and n3[1] == -1:
+        #if n4 == '-0' and
+
+    #Out.write()
+    In.close()
+    Out.close()
