7c11cde7be6ccf75c13e35921c2d280959a8f7e2
Mihlch <miki171028@gmail.com>
2021-02-07 12:14:30 +0700

    fff

--- bot.py
+++ bot.py
@@ -1,5 +1,14 @@
 import sys
 
+def Hod(prev, result):
+    if prev == "00":
+        return "-0"
+    else:
+        if result == 'a' or result == 'e':
+            return prev
+        else:
+            return "+0"
+
 if __name__ == '__main__':
     In = open(sys.argv[1], 'r')
     Out = open(sys.argv[2], 'w')
@@ -7,14 +16,12 @@ if __name__ == '__main__':
     n2 = In.readline()
     n3 = In.readline()
     n4 = In.readline()
-    print(n3, n4, "gggggggggggg")
     n5 = In.readline()
     n6 = In.readline()
-    n7 = In.readline()
-    hod =(0, 0)
-    #if n3[0] == -1 and n3[1] == -1:
-        #if n4 == '-0' and
-
-    #Out.write()
+    n7 = In.readline().split()
+    hod = Hod(n4, n5)
+    n7 = str(n7)
+    hod = str(hod)
+    Out.write(hod + n6 + n7)
     In.close()
     Out.close()
