8a259ec3b49442c46bda37d12386cfc6e0e01529
Mihlch <miki171028@gmail.com>
2021-02-21 12:10:28 +0700

    progress

--- bot.py
+++ bot.py
@@ -1,29 +1,46 @@
 import sys
 
-def Hod(prev, result):
+def Hod(prev, result, raz, nhod, pam):
     if prev == "00":
         return "+0"
     else:
-        if result == 'w' or result == 'r':
-            return "0+"
+        if (result == 'w' or result == 'r') and (prev == '+0' and int(nhod) < int(raz)):
+            return "0-"
+        elif (result == 'w' or result == 'r') and prev == '0-':
+            return "-0"
+        elif (result == 'w' or result == 'r') and prev == '-0':
+            return '0+'
+        elif prev == '0+':
+            return '+0'
+        elif prev == '+0' and pam[nhod-2] == '0+':
+            return '-0'
         else:
             return prev
+
+def List_to_streing(l):
+    s = ''
+    for i in l:
+        s = s + i + ' '
+    return s
+
 
 if __name__ == '__main__':
     In = open(sys.argv[1], 'r')
     Out = open(sys.argv[2], 'w')
-    n1 = In.readline()
-    n2 = In.readline()
-    n3 = In.readline()
-    n4 = In.readline()
-    n5 = In.readline()
-    n6 = In.readline()
-    n7 = In.readline().split()
-    hod = Hod(n4, n5)
-    n7 = str(n7)
+    fin = [s.strip() for s in In]
+    n1 = fin[0]
+    n2 = fin[1]
+    n3 = fin[2]
+    n4 = fin[3]
+    n5 = fin[4]
+    n6 = fin[5]
+    n7 = fin[6]
+    n7 = n7.split()
+    hod = Hod(n4, n5, n1, n2, n7)
     hod = str(hod)
-    Out.write(hod)
-    Out.write(n6)
-    Out.write(n7)
+    n7.append(hod)
+    Out.write(hod + '\n')
+    Out.write(n6 + '\n')
+    Out.write(List_to_streing(n7))
     In.close()
     Out.close()
